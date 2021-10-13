0dc3c725249ce3f75efe2ab67e957afbf025625f
Tiilia <dosimontp@gmail.com>
2021-10-13 16:19:07 +0200

    databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'OpenClassRoomsAngularTuto';
+  isAuth: boolean = false;
+
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    )
+  }
+  onAllumer() {
+    console.log('On allume tout !');
+  }
 }
