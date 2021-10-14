9d0b884c42a1e2311bd4199712108cdc179f0401
Tiilia <dosimontp@gmail.com>
2021-10-14 08:11:27 +0200

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isAuth: boolean = false;
+  lastUpdate = new Date();
 
-  appareilOne = 'Machine à laver';
-  appareilTwo = 'Frigo';
-  appareilThree = 'Ordinateur';
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ]
 
   constructor() {
     setTimeout(
